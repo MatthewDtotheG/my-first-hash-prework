3e096766dcae3b32d5a00a155ccd4825fd3dd265
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-18 02:27:38 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,8 +23,7 @@ def adding
 
   #your code here
   #remember to return the shipping_manifest hash
-  shipping_manifest["muskets"] = 2
-  shipping_manifest["gun powder"] = 4
+
 
   return shipping_manifest
 end
